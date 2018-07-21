845bbd306806e1072656428c78a0d6bd84839ce8
EvanYi <yyiu1199@163.com>
2018-07-21 21:28:17 +0800

    update home page

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const koa = require('koa');
-const app=new koa();
+const app = new koa();
+const { normal } = require('./tpl')
 
-app.use((ctx,next)=>{
-    ctx.body='电影首页';
+app.use((ctx, next) => {
+    ctx.type = 'text/html; charset=utf-8'
+    ctx.body = normal;
 })
 app.listen(3000);
