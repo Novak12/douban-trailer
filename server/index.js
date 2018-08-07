b6072a9db7d2c1f3210f01df9e6d8fe530405ea8
Novak12 <novak12@163.com>
2018-08-07 22:46:23 +0800

    add mongodb init file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const koa = require('koa');
 const app = new koa();
 const views = require('koa-views');
 const { resolve } = require('path');
+const { connect } = require('./database/init');
+
+; (async () => {
+    await connect();
+})();
 
 app.use(views(resolve(__dirname, './views'), {
     extension: 'pug'
